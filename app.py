82d8aa8b672efcf4e8330b83a42f2a2f904ef0b4
EliRoman <eli.j.roman@gmail.com>
2024-04-23 13:26:21 -0400

    Intial functionality for user registration page
    Form created.
    in the /register page there is now a form that takes a username and password and a button to POST the input.
    In app.py this input is added to a dictionary of users for now.
    No database implemented so far.

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 #second part of main.py in set-up video
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 
 from flask_sqlalchemy import SQLAlchemy
 from __init__ import app,db
 #from .models import Item
 # ^^from .models import [Class_Name]
+
+users = {}
+print(users)
 
 @app.route("/")
 def homepage():
@@ -14,9 +17,21 @@ def homepage():
 def login():
     return render_template("login.html")
 
-@app.route("/register")
+@app.route("/register", methods=['GET','POST'])
 def register():
-    return render_template("register.html")
+    if request.method == 'POST':
+        username=request.form['username']
+        print(f'requesting username: {username}')
+        if username in users:
+            print('username already exists')
+            return render_template("register.html")
+        else:
+            users[username] = request.form['password']
+            print(f'user {username} created')
+            print(users)
+            return render_template('homepage.html')
+    else:
+        return render_template('register.html')
 
 @app.route("/deposit")
 def deposit():
